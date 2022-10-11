b279976c2d424dc304d3d7d2db57e02be049c8c8
Danny <dl667@cornell.edu>
2022-10-11 18:44:04 -0400

    got fetch to work initially and working on displaying data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,47 @@
 // DEPENDENCIES
 var searchInputEl = document.getElementById("city-search");
 var searchButtonEl = document.getElementById("search-button");
+var currentDataEl = document.getElementsByClassName("current-data");
 
 // DATA
 var APIKey = "fc22c201b21fb998ddae41f7cecb6958";
-var city = "Chicago";
-var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
+var city;
+var queryURL;
 
 // FUNCTIONS
     // Fetch API
-function getWeather(queryURL) {
-    fetch(queryURL)
-        .then(function (response) {
-            console.log(response);
-            return response.json();
-        });
-};
-
-getWeather(queryURL);
-    // Search city weather data
+        // Search city weather data
     // Display info on dashboard/page
         // Current Weather Container:
             // City, date, weather icon in header
             // Temp, wind, humidity
         // 5-day forecast:
             // Dark grey cards with date on top, weather icon, tmep, wind, humidity
+function getWeather(queryURL) {
+    fetch(queryURL)
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            var temperature = (data.main.temp - 273.15) * 9/5 + 32;
+            temperature = temperature.toFixed(2);
+            console.log(temperature);
+            currentDataEl.value = "Temp: " + temperature + "°F";
+        });
+};
+
     // Save city to local storage
         // Input value becomes last city searched?
-
 
 // USER INTERACTIONS
     // User inputs location into search box
     // User clicks on city in saved searches list
 
 // INITIALIZATION
-
-
-// Notes
-    // Weather dashboard with FORM INPUTS
-    // Search for city (API, database)
-        // Presented with data + city is added to search history
-        // City name, date, icon representation of weather conditions, temperature, humidity, and wind speed
-        // 5-day forecast that displays date, icon representation of weather conditions, temperature, wind speed, and humidity
-    // When user clicks on city in search history, presented data for same city again
-
-    // API Call
-        // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-    // API Key
-        // fc22c201b21fb998ddae41f7cecb6958
+searchButtonEl.addEventListener("click", function() {
+    city = searchInputEl.value;
+    queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    getWeather(queryURL);
+});
