afecc8eed89ce38cf72583120608fff8ab83fe56
Danny <dl667@cornell.edu>
2022-10-11 19:41:47 -0400

    got dates to dispaly dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,33 @@
 // DEPENDENCIES
 var searchInputEl = document.getElementById("city-search");
 var searchButtonEl = document.getElementById("search-button");
-var currentDataEl = document.getElementsByClassName("current-data");
+var currentDataEl = document.querySelector(".current-data");
+var currentDateEl = document.querySelector(".current-weather-container");
+var forecastContainer = document.querySelector(".five-day-forecast-container");
+
 
 // DATA
 var APIKey = "fc22c201b21fb998ddae41f7cecb6958";
 var city;
 var queryURL;
+    // Date
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+
+currentDateEl.children[0].innerHTML = "(" + today + ")";
+console.log(today);
+
+for (let i = 0; i < forecastContainer.children.length; i++) {
+    newDay = new Date();
+    newDate = String(newDay.getDate()+i+1).padStart(2, '0');
+    forecastContainer.children[i].children[0].innerHTML = mm + "/" + newDate + "/" + yyyy;
+    console.log(newDate);
+}
+
 
 // FUNCTIONS
     // Fetch API
@@ -23,13 +44,26 @@ function getWeather(queryURL) {
             console.log(response);
             return response.json();
         })
+        // Current weather data
         .then(function (data) {
-            console.log(data);
+            var liTemp = document.createElement("li");
+            var liWind = document.createElement("li");
+            var liHumidity = document.createElement("li");
             var temperature = (data.main.temp - 273.15) * 9/5 + 32;
             temperature = temperature.toFixed(2);
-            console.log(temperature);
-            currentDataEl.value = "Temp: " + temperature + "°F";
-        });
+            var wind = data.wind.speed;
+            var humidity = data.main.humidity;
+            liTemp.innerHTML = "Temp: " + temperature + "°F";
+            liWind.innerHTML = "Wind: " + wind + " MPH";
+            liHumidity.innerHTML = "Humidity: " + humidity + " %";
+            currentDataEl.appendChild(liTemp);
+            currentDataEl.appendChild(liWind);
+            currentDataEl.appendChild(liHumidity);
+        })
+        // Forecast
+        .then(function (data) {
+
+        })
 };
 
     // Save city to local storage
